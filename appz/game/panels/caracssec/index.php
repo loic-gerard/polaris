b739ff5f992205927608cb9e3655f01e0814b4ef
Loïc Gerard <lgerard@diatem.net>
2015-01-16 08:02:48 +0100

    Developpement

--- appz/game/panels/caracssec/index.php
+++ appz/game/panels/caracssec/index.php
@@ -3,8 +3,10 @@
 use polarisapi\ui\categorie\VerticalList;
 use polarisapi\ui\categorie\BlocList;
 
+$pane = new VerticalList('CARACSEC', $selectedPlayer);
+echo $pane->build();
+
 $pane = new VerticalList('SEUILSBLESSURES', $selectedPlayer);
 echo $pane->build();
 
-$pane = new VerticalList('CARACSEC', $selectedPlayer);
-echo $pane->build();
+
--- config_pc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'polaris');
-define('DB_USER', 'polaris');
-define('DB_PASS', 'devadmin');
-define('DB_PORT', 5432);
-
